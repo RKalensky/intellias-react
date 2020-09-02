1a311def0e86ebabbd3f741c7ebad3eb6eda5674
roma.kalensky <roma.kalensky@gmail.com>
2020-09-02 20:49:26 +0300

    add(component): NoContent, to show that content with match query isn't exist

---
+++ src/components/NoContent/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+export default () => (
+  <h1 className="not-found text-center w-100">
+    The requested content
+    <br />
+    was not found.
+  </h1>
+);
