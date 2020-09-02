3919348104c85ffe8dbc4e156c4b8d09f3382a0d
roma.kalensky <roma.kalensky@gmail.com>
2020-09-02 22:52:12 +0300

    update(service): incorrect APIUrl resolve

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const APIUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEV_BACKEND_URL : process.env.REACT_APP_PROD_BACKEND_URL;
+const APIUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_BACKEND_URL : process.env.REACT_APP_DEV_BACKEND_URL;
 
 export default async (apiRoute, queryObject = {}) => axios(`${APIUrl}${apiRoute}`, { params: queryObject });
