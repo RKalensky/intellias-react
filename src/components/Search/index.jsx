f91f82a0df8f325fa89f9fb733b3e5aa20b059e3
roma.kalensky <roma.kalensky@gmail.com>
2020-09-02 23:50:45 +0300

    update(component): Search, get rid of classes

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 
 const Search = ({ onChangeHandler, currentValue }) => (
-  <Form className="ml-4 w-25" onSubmit={(event) => event.preventDefault()}>
+  <Form onSubmit={(event) => event.preventDefault()}>
     <Form.Control type="text" placeholder="Search products" value={currentValue} onChange={({ target: { value } }) => onChangeHandler(value)} />
   </Form>
 );
